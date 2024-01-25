ec2a8e13eeed74b392a1d909252171acc1927468
Tatiana Sugina <internetova@yandex.ru>
2024-01-25 14:49:12 +0300

    feat: updated YandexMapkitPlugin java

--- android/src/main/java/com/unact/yandexmapkit/YandexMapkitPlugin.java
+++ android/src/main/java/com/unact/yandexmapkit/YandexMapkitPlugin.java
@@ -1,7 +1,5 @@
 package com.unact.yandexmapkit;
 
-import android.content.Context;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.lifecycle.Lifecycle;
@@ -12,21 +10,11 @@ import io.flutter.embedding.engine.plugins.activity.ActivityAware;
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
 import io.flutter.embedding.engine.plugins.lifecycle.FlutterLifecycleAdapter;
 import io.flutter.plugin.common.BinaryMessenger;
-import io.flutter.plugin.common.MethodChannel;
 
 public class YandexMapkitPlugin implements FlutterPlugin, ActivityAware {
   private static final String VIEW_TYPE = "yandex_mapkit/yandex_map";
-  private static final String SEARCH_CHANNEL_ID   = "yandex_mapkit/yandex_search";
-  private static final String SUGGEST_CHANNEL_ID  = "yandex_mapkit/yandex_suggest";
-  private static final String DRIVING_CHANNEL_ID  = "yandex_mapkit/yandex_driving";
-  private static final String BICYCLE_CHANNEL_ID  = "yandex_mapkit/yandex_bicycle";
 
   @Nullable private Lifecycle lifecycle;
-
-  @Nullable private MethodChannel searchMethodChannel;
-  @Nullable private MethodChannel suggestMethodChannel;
-  @Nullable private MethodChannel drivingMethodChannel;
-  @Nullable private MethodChannel bicycleMethodChannel;
 
   @Override
   public void onAttachedToEngine(FlutterPluginBinding binding) {
@@ -34,8 +22,6 @@ public class YandexMapkitPlugin implements FlutterPlugin, ActivityAware {
 
     BinaryMessenger messenger = binding.getBinaryMessenger();
     binding.getPlatformViewRegistry().registerViewFactory(VIEW_TYPE, new YandexMapFactory(messenger, new LifecycleProvider()));
-
-    setupChannels(messenger, binding.getApplicationContext());
   }
 
   @Override
@@ -43,38 +29,8 @@ public class YandexMapkitPlugin implements FlutterPlugin, ActivityAware {
     teardownChannels();
   }
 
-  private void setupChannels(BinaryMessenger messenger, Context context) {
-    searchMethodChannel = new MethodChannel(messenger, SEARCH_CHANNEL_ID);
-    YandexSearch yandexSearch = new YandexSearch(context, messenger);
-    searchMethodChannel.setMethodCallHandler(yandexSearch);
-
-    suggestMethodChannel = new MethodChannel(messenger, SUGGEST_CHANNEL_ID);
-    YandexSuggest yandexSuggest = new YandexSuggest(context, messenger);
-    suggestMethodChannel.setMethodCallHandler(yandexSuggest);
-
-    drivingMethodChannel = new MethodChannel(messenger, DRIVING_CHANNEL_ID);
-    YandexDriving yandexDriving = new YandexDriving(context, messenger);
-    drivingMethodChannel.setMethodCallHandler(yandexDriving);
-
-    bicycleMethodChannel = new MethodChannel(messenger, BICYCLE_CHANNEL_ID);
-    YandexBicycle yandexBicycle = new YandexBicycle(context, messenger);
-    bicycleMethodChannel.setMethodCallHandler(yandexBicycle);
-  }
-
   @SuppressWarnings({"ConstantConditions"})
-  private void teardownChannels() {
-    searchMethodChannel.setMethodCallHandler(null);
-    searchMethodChannel = null;
-
-    suggestMethodChannel.setMethodCallHandler(null);
-    suggestMethodChannel = null;
-
-    drivingMethodChannel.setMethodCallHandler(null);
-    drivingMethodChannel = null;
-
-    bicycleMethodChannel.setMethodCallHandler(null);
-    bicycleMethodChannel = null;
-  }
+  private void teardownChannels() {}
 
   @Override
   public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
