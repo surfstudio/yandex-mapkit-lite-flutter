0e3449f4b9b9501256c13dfb6918725770899598
Tatiana Sugina <internetova@yandex.ru>
2024-01-25 14:50:46 +0300

    feat: add examples

---
+++ example/android/app/src/main/java/com/unact/yandexmapkitexample/MainActivity.java
@@ -0,0 +1,9 @@
+package com.unact.yandexmapkitexample;
+
+import androidx.annotation.NonNull;
+
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+
+public class MainActivity extends FlutterActivity {
+}
