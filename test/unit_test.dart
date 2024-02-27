9c787d1c6dd7d80032954948366f9442ebf9fb20
Alex Martynov <martynov.alex@gmail.com>
2024-02-27 13:01:44 +0600

    chore: add a test for fixing CI

---
+++ test/unit_test.dart
@@ -0,0 +1,10 @@
+// This is an example unit test.
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('Plus Operator', () {
+    test('should add two numbers together', () {
+      expect(1 + 1, 2);
+    });
+  });
+}
